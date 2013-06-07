e87c70ee277ecfc5374cc7bb9fc8116017ee10db
Ryan <rpallarino3@gmail.com>
2013-06-07 14:53:43 -0500

    FIlepath changes

--- src/playerstuff/TestPlayer.java
+++ src/playerstuff/TestPlayer.java
@@ -2,11 +2,13 @@ package playerstuff;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
+//import java.io.File;
 //import Sprites;
 
 public class TestPlayer {
-	public String player = "testplayer.png";
-	private String pathToPlayer = "Sprites/testplayer.png";
+	//public String player = "testplayer.png";
+	//File dir1 = new File(".");
+	private String pathToPlayer = "src/Sprites/testplayer.png";
 	private int x;
 	private int y;
 	private int dx;
@@ -14,7 +16,12 @@ public class TestPlayer {
 	private Image image;
 	
 	public TestPlayer() {
-		ImageIcon ii = new ImageIcon(player);
+//		try {
+//		System.out.println(dir1.getCanonicalPath());
+//		} catch (Exception e) {
+//			System.out.println("Nothing");
+//		}
+		ImageIcon ii = new ImageIcon(pathToPlayer);
 		image = ii.getImage();
 		x = 40;
 		y = 60;
